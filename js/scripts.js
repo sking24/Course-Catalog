413cf8a37b7bb96bb2c7e8289edba9fce9e1986b
King <sking24@vcd-ch204-06.eastern.ewu.edu>
2016-11-08 14:32:54 -0800

    version .5

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
-// YOUR SCRIPTS
+// GRID VIEW 
+
+$('#grid').click( function(){
+    
+    // Remove ONLY 'list' class
+    
+$('.cd-gallery').removeClass('list');   
+    
+    // add ONLY 'grid' class
+$('.cd-gallery').addClass('grid');
+
+
+});
+
+// LIST VIEW 
+
+$('#list').click( function(){
+    
+    // Remove ONLY 'list' class
+    
+$('.cd-gallery').removeClass('grid');   
+    
+    // add ONLY 'grid' class
+$('.cd-gallery').addClass('list');
+    
+});
